6dafb2c4e5864673b609714d02b1815a4c3261ee
volubepossum <domj.roli@gmail.com>
2024-11-24 16:34:55 +0100

    make teleop installable

--- startup.bash
+++ startup.bash
@@ -4,12 +4,6 @@ echo "Running startup script"
 
 # Source ROS2 workspace and run another script in the same subprocess
 source /opt/ros/rolling/setup.bash
+source ros2_ws/install/setup.bash
 
-# source ~/camera_ws/install/setup.bash
-# ros2 run camera_ros camera_node --ros-args -p camera:=0 -p format:='YUYV' -p width:=1920 -p height:=1080
-
-# source ./imu_ws/install/setup.bash 
-# ros2 launch tm_imu imu.launch.py
-
-source ./i2c_pwm_board/install/setup.bash
-ros2 run i2c_pwm_board node
+ros2 run teleop teleop_node
