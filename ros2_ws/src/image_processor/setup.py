ffed2a2b414ef946c75abfc5e18d19c940464a38
volubepossum <domj.roli@gmail.com>
2025-03-18 10:19:06 +0100

    refactor image processing node to use parameters for configuration and add launch file

--- ros2_ws/src/image_processor/setup.py
+++ ros2_ws/src/image_processor/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'image_processor'
@@ -7,19 +9,26 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include the config directory
+        (os.path.join('share', package_name, 'config'), 
+         glob('config/*.yaml')),
+        # Include the launch directory
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your.email@example.com',
-    description='get distance of the laser',
-    license='Apache License 2.0',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Image processing package for laser dot tracking',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'image_processor = image_processor.image_processor_node:main',
+            'image_processor_node = image_processor.image_processor_node:main',
         ],
     },
 )
